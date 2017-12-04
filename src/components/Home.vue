a224abd22b59f476e5e4fcd3dc6f23eee7815d15
Thijs Nijhof <tnijhof91@gmail.com>
2017-12-04 16:18:23 +0000

    add device screen

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,34 +1,44 @@
 <template>
-  <div class="hello">
-    <h1>{{msg}}</h1>
+  <div class="container">
+    <app-device></app-device>
+    <app-device></app-device>
+    <app-device></app-device>
+    <app-device></app-device>
+    <app-device></app-device>
+    <app-device></app-device>
   </div>
 </template>
 
 <script>
+  import Device from './listitems/Device.vue';
+
 export default {
   name: 'Home',
   data () {
     return {
       msg: 'Welcome to the home page'
     }
+  },
+  components: {
+    appDevice: Device
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+ .container {
+   display: -webkit-box;
+   display: -ms-flexbox;
+   display: flex;
+   -webkit-box-orient: horizontal;
+   -webkit-box-direction: normal;
+   -ms-flex-direction: row;
+   flex-direction: row;
+   -webkit-box-flex: 1;
+   -ms-flex: 1 1 auto;
+   justify-content: flex-start;
+   align-items:flex-start;
+   flex-wrap: wrap;
+ }
 </style>
---
+++ src/components/listitems/Device.vue
@@ -0,0 +1,72 @@
+<template>
+  <section class="device-container">
+    <div class="device-top">
+      <div class="active-icon"></div>
+      <h1 class="device-top-text">DEVICE_NAME</h1>
+    </div>
+    <div class="device-content">
+        <ul class="properties">
+          <li>last online - now</li>
+          <li>location: example_location</li>
+          <li>masterSlaveId: 123456789</li>
+        </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  .device-container {
+    display: flex;
+    flex-direction: column;
+    background: #fff;
+    justify-content: flex-start;
+    margin:5px;
+    min-width:25%;
+    flex: 1;
+    border:2px solid deepskyblue;
+    box-shadow: 2px 2px 2px #222;
+  }
+
+  .device-top {
+    display:flex;
+    flex-direction:row;
+    align-items:center;
+    justify-content: flex-start;
+    border-bottom: 1px solid deepskyblue;
+  }
+
+  .device-content {
+    display:flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-content: flex-start;
+  }
+
+  .device-top-text {
+    font-weight:100;
+    font-size:medium;
+  }
+  .properties {
+    display:flex;
+    flex-direction:column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    align-self:flex-start;
+    list-style: none;
+    font-weight:100;
+    font-size:small;
+  }
+
+  .active-icon {
+    height:15px;
+    width:15px;
+    background-color: limegreen;
+    border-radius:50%;
+    margin-left: 5%;
+    margin-right:5%;
+  }
+</style>
